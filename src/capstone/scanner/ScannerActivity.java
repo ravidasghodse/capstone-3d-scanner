d982f77a7945bfae062cc37755e95baf81dcecc1
lizhiheng.sjtu <lizhiheng.sjtu@gmail.com>
2011-11-17 11:27:16 +0000

    A new start

---
+++ src/capstone/scanner/ScannerActivity.java
@@ -0,0 +1,13 @@
+package capstone.scanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ScannerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
